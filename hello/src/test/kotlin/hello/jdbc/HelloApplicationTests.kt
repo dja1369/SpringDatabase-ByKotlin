d5461c140e3caeb4f61ac9cb3df05c4e5590ccde
LDS <vpfmzmtm@deepnoid.com>
2023-12-19 15:27:51 +0900

    first commit

---
+++ hello/src/test/kotlin/hello/jdbc/HelloApplicationTests.kt
@@ -0,0 +1,13 @@
+package hello.jdbc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HelloApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
